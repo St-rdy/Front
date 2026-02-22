f74f7a35ccc033fb23009da7a209608d24d69833
lee0806 <leeseh0806@gmail.com>
2026-02-22 19:07:01 +0900

    [FRONTEND-17] footer 디자인 작업 및 추가 라이브러리 확인
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/layouts/footer/Footer.test.tsx
+++ src/layouts/footer/Footer.test.tsx
@@ -5,7 +5,19 @@ import Footer from './footer'
 describe('Footer 테스트', () => {
   it('Footer 컴포넌트가 랜더링 되어야 한다.', () => {
     render(<Footer />)
-    const footerElement = screen.getByText('footer')
+    const footerElement = screen.getByText('커뮤니티')
     expect(footerElement).toBeInTheDocument()
+  })
+  it('Footer 컴포넌트에 5개의 버튼이 있어야 한다.', () => {
+    render(<Footer />)
+    const buttons = screen.getAllByRole('button')
+    expect(buttons).toHaveLength(5)
+  })
+  it('각 버튼을 클릭하면 onClick 이벤트가 발생해야 한다.', () => {
+    render(<Footer />)
+    const buttons = screen.getAllByRole('button')
+    buttons.forEach(button => {
+      expect(button).toBeInTheDocument()
+    })
   })
 })
